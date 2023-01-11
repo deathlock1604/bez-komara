e95cd2dca12fda5812da79f3c5115821c5663b8f
deathlock1604 <110537330+deathlock1604@users.noreply.github.com>
2023-01-11 22:31:00 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,137 @@
+!(function ($) {
+  "use strict";
+  // Cookie notification
+  document
+    .getElementById("dismiss-cookie-alert")
+    .addEventListener("click", function () {
+      document.getElementById("cookie-alert").style.display = "none";
+    });
+
+  // Activate smooth scroll on page load with hash links in the url
+  $(document).ready(function () {
+    if (window.location.hash) {
+      var initial_nav = window.location.hash;
+      if ($(initial_nav).length) {
+        var scrollto = $(initial_nav).offset().top;
+        $("html, body").animate(
+          {
+            scrollTop: scrollto,
+          },
+          1500,
+          "easeInOutExpo"
+        );
+      }
+    }
+  });
+
+  $(document).on("click", ".mobile-nav-toggle", function (e) {
+    $("body").toggleClass("mobile-nav-active");
+    $(".mobile-nav-toggle i").toggleClass(
+      "icofont-navigation-menu icofont-close"
+    );
+  });
+
+  $(document).click(function (e) {
+    var container = $(".mobile-nav-toggle");
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+      if ($("body").hasClass("mobile-nav-active")) {
+        $("body").removeClass("mobile-nav-active");
+        $(".mobile-nav-toggle i").toggleClass(
+          "icofont-navigation-menu icofont-close"
+        );
+      }
+    }
+  });
+
+  // Navigation active state on scroll
+  var nav_sections = $("section");
+  var main_nav = $(".nav-menu, .mobile-nav");
+
+  $(window).on("scroll", function () {
+    var cur_pos = $(this).scrollTop() + 200;
+
+    nav_sections.each(function () {
+      var top = $(this).offset().top,
+        bottom = top + $(this).outerHeight();
+
+      if (cur_pos >= top && cur_pos <= bottom) {
+        if (cur_pos <= bottom) {
+          main_nav.find("li").removeClass("active");
+        }
+        main_nav
+          .find('a[href="#' + $(this).attr("id") + '"]')
+          .parent("li")
+          .addClass("active");
+      }
+      if (cur_pos < 300) {
+        $(".nav-menu ul:first li:first").addClass("active");
+      }
+    });
+  });
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+
+  $(".back-to-top").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      1500,
+      "easeInOutExpo"
+    );
+    return false;
+  });
+
+  // assortment carousel (uses the Owl Carousel library)
+  $(".assortment-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      900: {
+        items: 3,
+      },
+    },
+  });
+
+  // assortment details carousel
+  $(".assortment-details-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    items: 1,
+  });
+
+  // Init AOS
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out-back",
+      once: true,
+    });
+  }
+  $(window).on("load", function () {
+    aos_init();
+  });
+})(jQuery);
+
+// The form has been submitted
+
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  document.querySelector("form").style.display = "none";
+  document.querySelector("#form-message").style.display = "block";
+});
